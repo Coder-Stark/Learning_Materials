5186548cd8e029e1f1ab09af42b03dcfeadd5bb1
shivam kumar <shivamkumar789321@gmail.com>
2025-07-21 20:57:53 +0530

    UndefinedVsNotDefined

---
+++ JavaScriptInterviewQues/UndefinedVsNotDefined.js
@@ -0,0 +1,14 @@
+//UNDEFINED VS NOT-DEFINED
+
+//undefined
+//placeholder till not assigning a value
+console.log(a);                //undefined (declare first then console, untill undefined)
+var a = 10;
+
+var b;                         //undefined
+console.log(b);
+
+
+//not defined
+//access variable which are not in program (give reference error)
+console.log(c);                //reference err : c is not defined
